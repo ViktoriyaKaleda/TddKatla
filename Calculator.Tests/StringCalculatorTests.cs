9f60b1f37c1dae5c49e43c84589c44bb4201f780
Unknown <kaledaviktoriya@gmail.com>
2019-02-06 00:30:31 +0300

    Add solution files.

---
+++ Calculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Calculator;
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_NullString_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => new StringCalculator().Add(null));
+        }
+
+        [TestCase("", ExpectedResult = 0)]
+        [TestCase("1", ExpectedResult = 1)]
+        [TestCase("1,2", ExpectedResult = 3)]
+        [TestCase("1,2,3", ExpectedResult = 6)]
+        [TestCase("1\n2,3", ExpectedResult = 6)]
+        [TestCase("//;\n1;2", ExpectedResult = 3)]
+        [TestCase("//[***]\n1***2***3", ExpectedResult = 6)]
+        [TestCase("//[*][%]\n1*2%3", ExpectedResult = 6)]
+        [TestCase("//[**][%]\n1**2%3", ExpectedResult = 6)]
+        [TestCase("//[**][%][,]\n1**2%3,10", ExpectedResult = 16)]
+        public int Add_ValidData_ValidResult(string numbers)
+        {
+            return new StringCalculator().Add(numbers);
+        }
+
+        [Test]
+        public void Add_InvalidData_Throws()
+        {
+            Assert.Throws<FormatException>(() => new StringCalculator().Add("1,\n"));
+        }
+
+        [Test]
+        public void Add_NegativeNumber_Throws()
+        {
+            Assert.Throws<ArgumentException>(() => new StringCalculator().Add("-1,2"));
+        }
+
+        [Test]
+        public void Add_NegativeNumbers_Throws()
+        {
+            Assert.Throws<ArgumentException>(() => new StringCalculator().Add("-1,2,-3,-4"));
+        }
+
+        [TestCase("1,1001", ExpectedResult = 1)]
+        [TestCase("1,2000,2,1003", ExpectedResult = 3)]
+        public int Add_NumbersGreaterThan1000_IgnoringThem(string numbers)
+        {
+            return new StringCalculator().Add(numbers);
+        }
+    }
+}
---
+++ Calculator/StringCalculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (numbers == null)
+                throw new ArgumentNullException();
+
+            if (numbers == "")
+                return 0;
+
+            var stringNumbers = ParseInput(numbers);
+
+            int result = 0;
+            var negatives = new List<string>();
+            foreach (var stringNumber in stringNumbers)
+            {
+                int number = Convert.ToInt32(stringNumber);
+
+                if (number < 0)
+                {
+                    negatives.Add(stringNumber);
+                    continue;
+                }
+
+                if (number > 1000)
+                    continue;
+
+                result += number;
+            }
+
+            if (negatives.Any())
+            {
+                throw new ArgumentException($"Negatives not allowed: {string.Join(", ", negatives)}", nameof(numbers));
+            }
+
+            return result;
+        }
+
+        private string[] ParseInput(string numbers)
+        {
+            char defaultDelimiter = ',';
+
+            // checks if we have the first line which contains the specific delimiter
+            if (numbers.StartsWith("//"))
+            {
+                // the case when we have input format like “//[delimiter][delimiter]...\n[numbers…]”
+                if (numbers.Contains("["))
+                {
+                    var delimitersString = numbers.Substring(3, numbers.LastIndexOf(']') - 3).Replace("]", "");
+                    var delimiters = delimitersString.Split('[');
+
+                    numbers = numbers.Substring(numbers.IndexOf('\n') + 1);
+
+                    foreach (var delimiter in delimiters)
+                    {
+                        numbers = numbers.Replace(delimiter, defaultDelimiter.ToString());
+                    }
+                }
+                // the case when we have only one specified delimiter
+                else
+                {
+                    defaultDelimiter = numbers[2];
+                    numbers = numbers.Substring(numbers.IndexOf('\n') + 1);
+                }
+            }
+
+            return numbers.Replace('\n', defaultDelimiter).Split(defaultDelimiter);
+        }
+    }
+}
